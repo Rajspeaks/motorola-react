337a72e08bda9af107c8545af3a16ea3ef087559
Rajdeep Das <dasrajdeep0306@gmail.com>
2022-05-30 00:06:28 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import Main from './component/main';
+import Razr_family from './component/razr_family';
+import Mototab from './component/mototab';
+import Mototab2 from './component/mototab2';
+import Smart_product from './component/smart_product';
 import { BrowserRouter, Switch, Route, Routes, Redirect} from 'react-router-dom';
 
 function App() {
@@ -9,6 +13,14 @@ function App() {
        <BrowserRouter>
           <Routes>
             <Route exact path ="/" element={<Main />}></Route>
+			<Route exact path ="/razr_family" element={<Razr_family />}></Route>
+			<Route exact path ="/mototab" element={<Mototab />}></Route>
+      <Route exact path ="/mototab2" element={<Mototab2 />}></Route>
+			<Route exact path ="/smart_product" element={<Smart_product />}></Route>
+          
+
+
+
           </Routes>
         </BrowserRouter>
     </div>
