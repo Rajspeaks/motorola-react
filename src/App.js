f4578b1491c2a153305dda3c657e056bddc4fd9a
Rajdeep Das <dasrajdeep0306@gmail.com>
2022-09-20 01:25:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Razr_family from './component/razr_family';
 import Razr_home from './component/razr_home';
 import Edge_family from './component/edge_family';
 import G_family from './component/g_family';
-import G22_Home from './component/g22_home';
+// import G22_Home from './component/g22_home';
 import Morefeatures from './component/morefeatures';
 import Mototab from './component/mototab';
 import Mototab2 from './component/mototab2';
@@ -21,13 +21,13 @@ function App() {
     <div className="App">
        <BrowserRouter>
           <Routes>
-      <Route exact path ="/" element={<Main />}></Route>
+      <Route exact path ="https://rajspeaks.github.io/motorola-react" element={<Main />}></Route>
       <Route exact path ="/about" element={<About />}></Route>
-			<Route exact path ="/razr_family" element={<Razr_family />}></Route>
+      <Route exact path ="/razr_family" element={<Razr_family />}></Route>
       <Route exact path ="/razr_home" element={<Razr_home />}></Route>
       <Route exact path ="/edge_family" element={<Edge_family />}></Route>
       <Route exact path ="/g_family" element={<G_family />}></Route>
-      <Route exact path ="/g22_home" element={<G22_Home />}></Route>
+//       <Route exact path ="/g22_home" element={<G22_Home />}></Route>
       <Route exact path ="/morefeatures" element={<Morefeatures />}></Route>
 			<Route exact path ="/mototab" element={<Mototab />}></Route>
       <Route exact path ="/mototab2" element={<Mototab2 />}></Route>
